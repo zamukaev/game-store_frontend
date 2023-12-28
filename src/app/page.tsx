f6557de094b32205bef37f4c69aa76dd8b78be58
Dmitry Shishkin <79608355+ihopeyoucanfly@users.noreply.github.com>
2023-12-28 14:47:03 +0300

    add input-price component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { useState } from "react";
+
 import Button from "@/components/ui/button/Button";
+import InputPrice from "@/components/ui/inputPrice/InputPrice";
 
 export default function Home() {
+    const [price, setPrice] = useState<number>(0);
+
+    console.log(price);
+
     return (
         <main className="main">
-            <Button kind="gray">Перейти на главную</Button>
+            {/* <Button kind="gray">Перейти на главную</Button> */}
+            <InputPrice
+                placeholder="от 199"
+                value={price}
+                setValue={setPrice}
+            />
         </main>
     );
 }
