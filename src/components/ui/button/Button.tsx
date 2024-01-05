5529e4693637597cd0948d83452e1dd9b17f1b81
Dmitry Shishkin <ex3l4nt@gmail.com>
2024-01-05 20:35:10 +0300

    test pipeline

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -7,7 +7,7 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
     kind?: "default" | "gray" | "cart" | "small" | "clear";
 }
-
+//
 const Button: FC<ButtonProps> = ({
     children,
     kind = "default",
