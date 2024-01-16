0c76a7f856ac7201c8bcb5f9883d41cf99243cd7
zamukaev <abu.safiia2016@mail.com>
2024-01-16 22:52:46 +0100

    add filters, sorting, storybook

--- src/shared/ui/linkItem/LinkItem.test.tsx
+++ src/shared/ui/linkItem/LinkItem.test.tsx
@@ -5,37 +5,37 @@ import LinkItem, { Kind } from "./LinkItem";
 describe("NavItem Component", () => {
     test("NavItem renders correctly", () => {
         const { container } = render(
-            <LinkItem kind={Kind.EMAIL} text="some text" />
+            <LinkItem kind={Kind.EMAIL} >some text</LinkItem>
         );
         expect(container).toBeInTheDocument();
     });
     test("NavItem renders text correctly", () => {
         const { getByTestId } = render(
-            <LinkItem kind={Kind.EMAIL} text="some text" />
+            <LinkItem kind={Kind.EMAIL} >some text</LinkItem>
         );
         expect(getByTestId("linkItem")).toHaveTextContent("some text");
     });
     test("NavItem renders with the class email", () => {
         const { getByTestId } = render(
-            <LinkItem kind={Kind.EMAIL} text="some text" />
+            <LinkItem kind={Kind.EMAIL} >some text</LinkItem>
         );
         expect(getByTestId("linkItem")).toHaveClass("email");
     });
     test("NavItem renders with the class instagram", () => {
         const { getByTestId } = render(
-            <LinkItem kind={Kind.INSTAGRAM} text="some text" />
+            <LinkItem kind={Kind.INSTAGRAM} >some text</LinkItem>
         );
         expect(getByTestId("linkItem")).toHaveClass("instagram");
     });
     test("NavItem renders with the class phone", () => {
         const { getByTestId } = render(
-            <LinkItem kind={Kind.PHONE} text="some text" />
+            <LinkItem kind={Kind.PHONE} >some text</LinkItem>
         );
         expect(getByTestId("linkItem")).toHaveClass("phone");
     });
     test("NavItem renders with the class whatsapp", () => {
         const { getByTestId } = render(
-            <LinkItem kind={Kind.WHATSAPP} text="some text" />
+            <LinkItem kind={Kind.WHATSAPP} >some text</LinkItem>
         );
         expect(getByTestId("linkItem")).toHaveClass("whatsapp");
     });
