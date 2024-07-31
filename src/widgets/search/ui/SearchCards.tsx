6ba1f72df7d875b248c739dcab7b898e7fd53a93
Petrochenk0 <96386665+Petrochenk0@users.noreply.github.com>
2024-07-31 11:57:45 +0300

    bugfix/fixing adaptive goods and skeletons

--- src/widgets/search/ui/SearchCards.tsx
+++ src/widgets/search/ui/SearchCards.tsx
@@ -1,12 +1,10 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
-
 import { useQuery } from "@tanstack/react-query";
 
 import { GoBackButton, Headline, ProductCard } from "@/shared/ui";
 import ProductCardLoader from "@/shared/ui/productCard/ProductCardLoader";
-
 import SearchEmpty from "@/widgets/search/ui/SearchEmpty/SearchEmpty";
 
 import { fetchSearchResult } from "../api";
@@ -17,37 +15,41 @@ const SearchCards = () => {
     const searchParams = useSearchParams();
     const query = searchParams.get("query");
 
+    const [numCards, setNumCards] = useState(() => {
+        const width = window.innerWidth;
+        if (width < 768) return 1;
+        if (width < 1024) return 2;
+        if (width < 1650) return 3;
+        return 4;
+    });
+
     const { data, isLoading, error } = useQuery({
         queryKey: ["searchResults"],
         queryFn: () => fetchSearchResult(query as string),
         enabled: !!query,
     });
 
-    if (isLoading) {
-        return (
-            <>
-                <GoBackButton
-                    className={styles.link}
-                    href="/"
-                    text="Вернуться к покупкам"
-                />
-                <Headline className={styles.query_title}>{query}</Headline>
-                <div className={styles.loader}>
-                    {Array.from({ length: 4 }).map((_, index) => (
-                        <ProductCardLoader key={index} />
-                    ))}
-                </div>
-            </>
-        );
-    }
+    useEffect(() => {
+        const handleResize = () => {
+            const width = window.innerWidth;
+            if (width < 768) {
+                setNumCards(1);
+            } else if (width < 1024) {
+                setNumCards(2);
+            } else if (width < 1650) {
+                setNumCards(3);
+            } else {
+                setNumCards(4);
+            }
+        };
 
-    if (error) {
-        return <div>Ошибка</div>;
-    }
+        window.addEventListener("resize", handleResize);
+        handleResize();
 
-    if (!data || data.length === 0) {
-        return <SearchEmpty />;
-    }
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     return (
         <div>
@@ -61,11 +63,23 @@ const SearchCards = () => {
             </div>
 
             <div className={styles.product} data-testid="productCard">
-                <div className={styles.product__container}>
-                    {data.map((item: any) => (
-                        <ProductCard key={item._id} product={item} />
-                    ))}
-                </div>
+                {isLoading || !data ? (
+                    <div className={styles.loader}>
+                        {Array.from({ length: numCards }).map((_, index) => (
+                            <ProductCardLoader key={index} />
+                        ))}
+                    </div>
+                ) : error ? (
+                    <div>Ошибка</div>
+                ) : data.length === 0 ? (
+                    <SearchEmpty />
+                ) : (
+                    <div className={styles.product__container}>
+                        {data.map((item: any) => (
+                            <ProductCard key={item._id} product={item} />
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     );
